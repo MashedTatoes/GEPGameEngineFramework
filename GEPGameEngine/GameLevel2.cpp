c755fb45d9034a95ae2c89f9e13e0934bcf3c20a
Josh A <joshmail9510@gmail.com>
2020-10-29 15:54:54 -0400

    Added Level2 and Level3 as well as backgrounds

---
+++ GEPGameEngine/GameLevel2.cpp
@@ -0,0 +1,24 @@
+#include "GameLevel2.h"
+#include "Game.h"
+
+void GameLevel2::Enter()
+{
+
+	gameStart = SDL_GetTicks();
+	bgSpriteTex = Game::Instance()->LoadTexture("resources/images/bg_level2.png");
+	mainSpriteTex = Game::Instance()->LoadTexture("resources/images/PlayerKenSprite.png");
+
+	enemySpriteTex = Game::Instance()->LoadTexture("resources/images/PlayerKenSpriteEnemy.png");
+
+	SDL_Rect bgDestRect;
+	bgDestRect.x = bgDestRect.y = 0;
+	SDL_GetWindowSize(Game::Instance()->GetWindow(), &bgDestRect.w, &bgDestRect.h);
+	player = new Player(mainSpriteTex, bgDestRect.w * 0.5, bgDestRect.h - 75);
+
+	enemy = new Enemy(enemySpriteTex, bgDestRect.w * 0.75, bgDestRect.h - 75, player);
+	player->SetEnemy(enemy);
+
+	GameState::Enter();
+
+
+}
